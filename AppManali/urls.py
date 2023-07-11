3ae0e035d63c3e565d8a48dbca6775218e61e694
Juan Pablo <manalijuan@gmail.com>
2023-07-11 20:10:44 -0300

    l

--- AppManali/urls.py
+++ AppManali/urls.py
@@ -5,7 +5,6 @@ urlpatterns= [
     path("administrativo",views.administrativo, name="Administrativo"),
     path("doctor",views.doctor, name="Doctor"),
     path("paciente",views.paciente, name="Paciente"),
-    path("administrativoFormulario",views.administrativoFormulario, 
-         name="administrativoFormulario")
+    path("formulario",views.formulario, name="Formulario")
 
 ]
