607eaa8dab2e05f77728e7eb053ceace6d9f8f4a
Juan Pablo <manalijuan@gmail.com>
2023-07-13 14:34:27 -0300

    guardo

--- AppManali/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-class AdministrativoFormulario(forms.Form):
-    nombre=forms.CharField()
-    apellido=forms.CharField()
-
-class PacienteFormulario(forms.Form):
-    nombre=forms.CharField()
-    apellido=forms.CharField()
-    email=forms.EmailField()    
